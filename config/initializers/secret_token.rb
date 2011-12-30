4ef8f61dd7d67a2339f5f45cae43e99d4ba562a6
Dan Gebhardt <dan@cerebris.com>
2011-12-30 18:29:05 -0500

    initial commit with contacts scaffolded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberRestExample::Application.config.secret_token = '171c578b9a5e608e48e7ca3ec93f23bc7ee06ca3feffec451c209a91478c6085ce57decb773e1c58614e51153db7f504dc758d6f53c9248615c2ed2edcde7654'
