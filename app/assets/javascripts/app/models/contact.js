012cecc2324f1f089adb42a4ec3a38be4b4b9a2c
Dan Gebhardt <dan@cerebris.com>
2012-01-04 12:00:05 -0500

    reorganized methods

--- app/assets/javascripts/app/models/contact.js
+++ app/assets/javascripts/app/models/contact.js
@@ -3,12 +3,12 @@ App.Contact  = Ember.Resource.extend({
   name:       'contact',
   properties: ['first_name', 'last_name'],
 
+  validate: function(userData) {
+    if (userData.first_name === '' || userData.last_name === '')
+      return 'Contacts require a first and a last name.';
+  },
+
   fullName: Ember.computed(function() {
     return this.get('first_name') + ' ' + this.get('last_name');
-  }).property('first_name', 'last_name'),
-
-  validate: function(data) {
-    if (data.first_name === '' || data.last_name === '')
-      return 'Contacts require a first and a last name.';
-  }
+  }).property('first_name', 'last_name')
 });
