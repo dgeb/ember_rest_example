e1e2c5791ca8cd39f2c28b0200d4662729384423
Dan Gebhardt <dan@cerebris.com>
2012-01-04 12:15:29 -0500

    added comments and finer grained methods; changed method names to serialize() and deserialize()

--- app/assets/javascripts/lib/ember-rest.js
+++ app/assets/javascripts/lib/ember-rest.js
@@ -1,6 +1,6 @@
 // A model class for RESTful resources
 //
-// Extend this class and define the following:
+// Extend this class and define the following properties:
 //
 // * `name` -- the name used to contain the serialized data in this object's json
 //      representation
@@ -9,26 +9,66 @@
 // * `url` -- (optional) the base url of the resource (e.g. '/contacts/active');
 //      will default to the `url` for `type`
 //
+// You may also wish to override the following methods:
+//
+// * `serialize()`
+// * `serializeProperty(prop)`
+// * `deserialize(json)`
+// * `deserializeProperty(prop, value)`
+// * `validate(userData)`
+//
 Ember.Resource = Ember.Object.extend({
   name:       Ember.required(),
   properties: Ember.required(),
   url:        Ember.required(),
 
-  data: function() {
+  // Generate this resource's JSON representation
+  //
+  // Override this or `serializeProperty` to provide custom serialization
+  serialize: function() {
     var ret = {},
         prop;
 
     ret[this.name] = {};
     for(var i = 0; i < this.properties.length; i++) {
       prop = this.properties[i];
-      ret[this.name][prop] = this.get(prop);
+      ret[this.name][prop] = this.serializeProperty(prop);
     }
     return ret;
   },
 
-  update: function(data) {
+  // Generate an individual property's JSON representation
+  //
+  // Override to provide custom serialization
+  serializeProperty: function(prop) {
+    return this.get(prop);
+  },
+
+  // Set this resource's properties from JSON
+  //
+  // Override this or `deserializeProperty` to provide custom deserialization
+  deserialize: function(json) {
+    Ember.beginPropertyChanges(this);
+    for(var prop in json) {
+      if (json.hasOwnProperty(prop)) this.deserializeProperty(prop, json[prop]);
+    }
+    Ember.endPropertyChanges(this);
+    return this;
+  },
+
+  // Set an individual property from its value in JSON
+  //
+  // Override to provide custom serialization
+  deserializeProperty: function(prop, value) {
+    this.set(prop, value);
+  },
+
+  // Validate and then save a set of changes
+  //
+  // Provide a `validate(userData)` method to perform validations.
+  update: function(userData) {
     if (this.validate !== undefined) {
-      var error = this.validate(data);
+      var error = this.validate(userData);
       if (error) {
         return {
           fail: function(f) { f(error); return this; },
@@ -37,25 +77,31 @@ Ember.Resource = Ember.Object.extend({
         };
       }
     }
-    return this.setProperties(data).save();
+    return this.setProperties(userData).save();
   },
 
+  // Create (if new) or update (if existing) record via ajax
+  //
+  // If successful, updates this record's id and other properties
+  // by calling `deserialize()` with the data returned.
   save: function() {
     var self = this,
         isNew = (this.get('id') === undefined);
 
     return jQuery.ajax({
       url: this._url(),
-      data: this.data(),
+      data: this.serialize(),
       dataType: 'json',
       type: (isNew ? 'POST' : 'PUT'),
-      success: function(data) {
-        if (data)
-          self.setProperties(data);
+      success: function(json) {
+        // Update properties
+        if (json)
+          self.deserialize(json);
       }
     });
   },
 
+  // Delete record via ajax
   destroy: function() {
     var self = this;
 
@@ -66,14 +112,16 @@ Ember.Resource = Ember.Object.extend({
     });
   },
 
+  // The URL for this resource, base on `url` and `id`
+  // (which will be undefined for new resources).
   _url: function() {
     var url = this.url,
         id = this.get('id');
 
     if (id !== undefined)
-      return url + '/' + id;
-    else
-      return url;
+      url += '/' + id;
+
+    return url;
   }
 });
 
@@ -90,35 +138,52 @@ Ember.ResourceController = Ember.ArrayController.extend({
   type:     Ember.required(),
   content:  [],
 
+  // Create and load a single `Ember.Resource` from JSON
   load: function(json) {
-    this.pushObject(this.get('type').create(json));
+    var resource = this.get('type').create().deserialize(json);
+    this.pushObject(resource);
   },
 
-  loadAll: function(jsonArray) {
-    for (var i=0; i < jsonArray.length; i++)
-      this.load(jsonArray[i]);
+  // Create and load `Ember.Resource` objects from a JSON array
+  loadAll: function(json) {
+    for (var i=0; i < json.length; i++)
+      this.load(json[i]);
   },
 
+  // Replace this controller's contents with an ajax call to `url`
   findAll: function() {
     var self = this;
 
-    jQuery.getJSON(this._url(), function(data) {
-      self.set("content", []);
-      self.loadAll(data);
+    return jQuery.ajax({
+      url: this._url(),
+      dataType: 'json',
+      type: 'GET',
+      success: function(json) {
+        self.set("content", []);
+        self.loadAll(json);
+      }
     });
   },
 
-  create: function(data) {
+  // Given data entered by a user, create a new resource with ajax
+  //
+  // `Ember.Resource.update()` will validate the user-entered data.
+  // If validation and the ajax call are successful, the new resource
+  // will be added to this controller's contents.
+  create: function(userData) {
     var self = this;
     var resource = this.get('type').create();
 
     return resource
-      .update(data)
+      .update(userData)
         .done(function() {
           self.pushObject(resource);
         });
   },
 
+  // Delete a resource with ajax
+  //
+  // If successful, the resource will be removed from this controller's contents.
   destroy: function(resource) {
     var self = this;
 
@@ -129,6 +194,10 @@ Ember.ResourceController = Ember.ArrayController.extend({
       });
   },
 
+  // Determine the base URL for ajax calls
+  //
+  // Will use the `url` set for this controller, or if that's missing,
+  // the `url` specified for `type`.
   _url: function() {
     if (this.url === undefined)
       return this.get('type').prototype.url;
