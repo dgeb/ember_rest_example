824f176c0f7c6d928ac26fc5aa793766edd6a6c3
Dan Gebhardt <dan@cerebris.com>
2012-01-05 22:15:16 -0500

    moved all record duplication / saving logic to edit view; the show view is now much cleaner

--- app/assets/javascripts/app/views/contacts/edit.js
+++ app/assets/javascripts/app/views/contacts/edit.js
@@ -1,6 +1,15 @@
 App.EditContactView = Ember.Form.extend({
   templateName: 'app/templates/contacts/edit',
 
+  init: function() {
+    // create a new contact that's a duplicate of the contact in the parentView;
+    // changes made to the duplicate won't be applied to the original unless
+    // everything goes well in submitForm()
+    var editableContact = App.Contact.create(this.get('parentView').get('contact'));
+    this.set("contact", editableContact);
+    this._super();
+  },
+
   cancelForm: function() {
     this.get("parentView").hideEdit();
   },
--- app/assets/javascripts/app/views/contacts/show.js
+++ app/assets/javascripts/app/views/contacts/show.js
@@ -9,27 +9,10 @@ App.ShowContactView = Ember.View.extend({
   },
 
   showEdit: function() {
-    if (this.get('isEditing') === true) return;
-
-    // create a duplicate contact record to be edited;
-    // if successful, the original will be replaced in hideEdit()
-    var editableContact = App.Contact.create(this.get('contact'));
-
-    // create a new edit view and append it to this view
-    this.editView = App.EditContactView.create({
-      contact: editableContact,
-      parentView: this
-    }).appendTo(this.$('.data'));
-
-    // set the flag to show/hide elements in the template
     this.set('isEditing', true);
   },
 
   hideEdit: function() {
-    // remove the edit view created above
-    this.editView.remove();
-
-    // set the flag to show/hide elements in the template
     this.set('isEditing', false);
   },
 
